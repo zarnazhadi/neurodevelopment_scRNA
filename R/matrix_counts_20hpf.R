1397f1287747ab8014aa16eee555b58d34416841
zxh755 <zxh755@student.bham.ac.uk>
2021-02-27 12:29:23 +0000

    changed min counts to 500 and updated mitochondrial data

--- R/matrix_counts_20hpf.R
+++ R/matrix_counts_20hpf.R
@@ -70,18 +70,18 @@ S2.dense.size/S2.sparse.size # 10.4 bytes
 
 # initialize the Seurat object with the raw (non-normalized data).  Keep all
 # genes expressed in >= 3 cells (~0.1% of the data). Keep all cells with at
-# least 200 detected genes
+# least 500 detected genes
 
-S1a <- CreateSeuratObject(counts = S1a.counts, min.cells = 3, min.features  = 200, project = "S1a", assay = "RNA")
-S1b <- CreateSeuratObject(counts = S1b.counts, min.cells = 3, min.features  = 200, project = "S1b", assay = "RNA")
-S2 <- CreateSeuratObject(counts = S2.counts, min.cells = 3, min.features  = 200, project = "S2", assay = "RNA")
+S1a <- CreateSeuratObject(counts = S1a.counts, min.cells = 3, min.features  = 500, project = "S1a", assay = "RNA")
+S1b <- CreateSeuratObject(counts = S1b.counts, min.cells = 3, min.features  = 500, project = "S1b", assay = "RNA")
+S2 <- CreateSeuratObject(counts = S2.counts, min.cells = 3, min.features  = 500, project = "S2", assay = "RNA")
 
 # QC and selecting cells for further analysis
 # % mitochondrial genes
 
-S1a.mito.genes <- grep(pattern = "^MT-", x = rownames(S1a@assays[["RNA"]]), value = TRUE)
-S1b.mito.genes <- grep(pattern = "^MT-", x = rownames(S1b@assays[["RNA"]]), value = TRUE)
-S2.mito.genes <- grep(pattern = "^MT-", x = rownames(S2@assays[["RNA"]]), value = TRUE)
+S1a.mito.genes <- grep(pattern = "mt-|^AC0", x = rownames(S1a@assays[["RNA"]]), value = TRUE)
+S1b.mito.genes <- grep(pattern = "mt-|^AC0", x = rownames(S1b@assays[["RNA"]]), value = TRUE)
+S2.mito.genes <- grep(pattern = "mt-|^AC0", x = rownames(S2@assays[["RNA"]]), value = TRUE)
 
 S1a.percent.mito <- Matrix::colSums(S1a@assays[["RNA"]][S1a.mito.genes, ])/Matrix::colSums(S1a@assays[["RNA"]])
 S1b.percent.mito <- Matrix::colSums(S1b@assays[["RNA"]][S1b.mito.genes, ])/Matrix::colSums(S1b@assays[["RNA"]])
@@ -91,16 +91,53 @@ S1a <- AddMetaData(object = S1a, metadata = S1a.percent.mito, col.name = "S1a.pe
 S1b <- AddMetaData(object = S1b, metadata = S1b.percent.mito, col.name = "S1b.percent.mito")
 S2 <- AddMetaData(object = S2, metadata = S2.percent.mito, col.name = "S2.percent.mito")
 
-# plots
-pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/20hpf_S1a_vln_plot.pdf")
+# vln plots
+pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/UPDATE_20hpf_S1a_vln_plot.pdf")
 VlnPlot(object = S1a, features = c("nFeature_RNA", "nCount_RNA", "S1a.percent.mito"), ncol = 3)
 dev.off()
 
-pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/20hpf_S1b_vln_plot.pdf")
+pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/UPDATE_20hpf_S1b_vln_plot.pdf")
 VlnPlot(object = S1b, features = c("nFeature_RNA", "nCount_RNA", "S1b.percent.mito"), ncol = 3)
 dev.off()
 
-pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/20hpf_S2_vln_plot.pdf")
+pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/UPDATE_20hpf_S2_vln_plot.pdf")
 VlnPlot(object = S2, features = c("nFeature_RNA", "nCount_RNA", "S2.percent.mito"), ncol = 3)
 dev.off()
 
+# scatter plots
+pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/20hpf_S1a_mito_scatter_plot.pdf")
+par(mfrow = c(1, 2))
+FeatureScatter(object = S1a, feature1 = "nCount_RNA", feature2 = "S1a.percent.mito")
+dev.off()
+
+pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/20hpf_S1a_RNA_scatter_plot.pdf")
+FeatureScatter(object = S1a, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+dev.off()
+
+pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/18hpf_S1b_mito_scatter_plot.pdf")
+par(mfrow = c(1, 2))
+FeatureScatter(object = S1b, feature1 = "nCount_RNA", feature2 = "S1b.percent.mito")
+dev.off()
+
+pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/18hpf_S1b_RNA_scatter_plot.pdf")
+FeatureScatter(object = S1b, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+dev.off()
+
+pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/18hpf_S2_mito_scatter_plot.pdf")
+par(mfrow = c(1, 2))
+FeatureScatter(object = S2, feature1 = "nCount_RNA", feature2 = "S2.percent.mito")
+dev.off()
+
+pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/18hpf_S2_RNA_scatter_plot.pdf")
+FeatureScatter(object = S2, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+dev.off()
+
+# filter
+S1a <- subset(x = S1a, subset = nFeature_RNA > 500 & nFeature_RNA < 2500 & S1a.percent.mito >  -Inf & S1a.percent.mito < 0.05 )
+S1b <- subset(x = S1b, subset = nFeature_RNA > 500 & nFeature_RNA < 2500 & S1b.percent.mito >  -Inf & S1b.percent.mito < 0.05 )
+S2 <- subset(x = S2, subset = nFeature_RNA > 500 & nFeature_RNA < 2500 & S2.percent.mito >  -Inf & S2.percent.mito < 0.05 )
+
+# normalise
+S1a <- NormalizeData(object = S1a, normalization.method = "LogNormalize", scale.factor = 10000)
+S1b <- NormalizeData(object = S1b, normalization.method = "LogNormalize", scale.factor = 10000)
+S2 <- NormalizeData(object = S2, normalization.method = "LogNormalize", scale.factor = 10000)
