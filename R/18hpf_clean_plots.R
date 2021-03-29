95956b7332073e5d4b88536a4ddbe24ff08833dc
zxh755 <zxh755@student.bham.ac.uk>
2021-03-29 11:47:33 +0100

    changed variable name from sample to s

--- R/18hpf_clean_plots.R
+++ R/18hpf_clean_plots.R
@@ -7,18 +7,21 @@ library(cowplot)
 # open clean data 
 setwd("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/data")
 
-sample.list <- list.files(pattern=glob2rx("*hpf18_clean.rds"))
+s.list <- list.files(pattern=glob2rx("*hpf18_clean.rds"))
 
-for(sample in 1:length(sample.list)){
-  sample.name <- substr(sample.list[sample], 1,3)
-  if(grepl(sample.name, pattern="_")){
-    sample.name <- substr(sample.list[sample], 1,2)
-  }
-  
-  sample.obj <- readRDS(sample.list[sample])
+for(s in 1:length(s.list)){
+  s.name <- substr(s.list[s], 1,3)
+    if(grepl(s.name, pattern="_")){
+    s.name <- substr(s.list[s], 1,2)
+    }
+  s.obj <- readRDS(s.list[s])
 # RNA scatter plot 
-  pdf(paste0("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/18hpf/pre-normalisation/CLEAN_", sample.name,"_RNA_scatter_plot.pdf"))
-  p <- FeatureScatter(object = sample.obj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
-  p
+  pdf(paste0("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/18hpf/pre-normalisation/CLEAN_", s.name, "_RNA_scatter_plot.pdf"))
+  FeatureScatter(object = s.obj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+  dev.off()
+  
+# VLN plot
+  pdf(paste0("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/18hpf/pre-normalisation/CLEAN_", s.name, "_mito_vln_plot.pdf"))
+  VlnPlot(object = s.obj, features = c("nFeature_RNA", "nCount_RNA", "percent.mito"), ncol = 3)
   dev.off()
 }
