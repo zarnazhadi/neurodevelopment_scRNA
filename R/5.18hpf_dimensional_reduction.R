cd482d34e2deba49e34950c098a0c5d563384bae
zxh755 <zxh755@student.bham.ac.uk>
2021-04-01 15:30:24 +0100

    played around with graphs

--- R/5.18hpf_dimensional_reduction.R
+++ R/5.18hpf_dimensional_reduction.R
@@ -5,22 +5,32 @@ library(dplyr)
 library(cowplot)
 
 setwd("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/data")
-s <- readRDS("hpf18.cluster.rds")
+sample <- readRDS("hpf18.cluster.rds")
 
-s <- RunTSNE(object = s, dims.use = 1:10, do.fast = TRUE)
+sample <- RunTSNE(object = sample, dims.use = 1:10, do.fast = TRUE)
 
 pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/18hpf/post-clustering/18hpf_tSNE_plot.pdf")
-DimPlot(object = s, reduction = "tsne")
+DimPlot(object = sample, reduction = "tsne")
 dev.off()
 
-s <- RunUMAP(s, reduction = "pca", dims = 1:20)
+sample <- RunUMAP(sample, reduction = "pca", dims = 1:20)
 
 # this doesnt work :(
-#pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/18hpf/post-clustering/18hpf_UMAP.pdf")
-#DimPlot(s, reduction = "umap", split.by = "seurat_clusters")
-#dev.off()
-
-# this works :)
 pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/18hpf/post-clustering/18hpf_UMAP.pdf")
-DimPlot(s, reduction = "umap", split.by = "orig.ident")
+DimPlot(sample, reduction = "umap", split.by = "seurat_clusters") + RotatedAxis()
+dev.off()
+
+pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/other/other.pdf")
+#DotPlot(sample, features = "nCount_RNA", split.by = "seurat_clusters") + RotatedAxis()
+
+#DoHeatmap(sample, features = VariableFeatures(sample)[1:100], cells = 1:500, size = 1, 
+          #angle = 90) + NoLegend()
+
+#DimPlot(sample)
+
+#sample.no.tsne <- sample
+#sample.no.tsne[["tsne"]] <- NULL
+#DimPlot(sample.no.tsne) + RotatedAxis()
+
+DimPlot(sample, reduction = "umap")
 dev.off()
