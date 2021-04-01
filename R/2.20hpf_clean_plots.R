4a76bb16662a3546843a2982755b7719193f982b
zxh755 <zxh755@student.bham.ac.uk>
2021-04-01 13:25:37 +0100

    added file

---
+++ R/2.20hpf_clean_plots.R
@@ -0,0 +1,67 @@
+library("Matrix")
+library("Seurat")
+library(tidyverse)
+library(dplyr)
+library(cowplot)
+
+# open clean data 
+setwd("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/data")
+
+s.list <- list.files(pattern=glob2rx("*hpf20_clean.rds"))
+
+# S1a
+s <- 1
+s.name <- substr(s.list[s], 1,3)
+if(grepl(s.name, pattern="_")){
+  s.name <- substr(s.list[s], 1,2)
+}
+
+s.obj <- readRDS(s.list[s])
+
+# RNA scatter plot 
+pdf(paste0("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/20hpf/pre-normalisation/CLEAN_", s.name, "_RNA_scatter_plot.pdf"))
+FeatureScatter(object = s.obj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+dev.off()
+
+# VLN plot
+pdf(paste0("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/20hpf/pre-normalisation/CLEAN_", s.name, "_mito_vln_plot.pdf"))
+VlnPlot(object = s.obj, features = c("nFeature_RNA", "nCount_RNA", "percent.mito"), ncol = 3)
+dev.off()
+
+# S1b
+s <- 2
+s.name <- substr(s.list[s], 1,3)
+if(grepl(s.name, pattern="_")){
+  s.name <- substr(s.list[s], 1,2)
+}
+
+s.obj <- readRDS(s.list[s])
+
+# RNA scatter plot 
+pdf(paste0("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/20hpf/pre-normalisation/CLEAN_", s.name, "_RNA_scatter_plot.pdf"))
+FeatureScatter(object = s.obj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+dev.off()
+
+# VLN plot
+pdf(paste0("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/20hpf/pre-normalisation/CLEAN_", s.name, "_mito_vln_plot.pdf"))
+VlnPlot(object = s.obj, features = c("nFeature_RNA", "nCount_RNA", "percent.mito"), ncol = 3)
+dev.off()
+
+# S2
+s <- 3
+s.name <- substr(s.list[s], 1,3)
+if(grepl(s.name, pattern="_")){
+  s.name <- substr(s.list[s], 1,2)
+}
+
+s.obj <- readRDS(s.list[s])
+
+# RNA scatter plot 
+pdf(paste0("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/20hpf/pre-normalisation/CLEAN_", s.name, "_RNA_scatter_plot.pdf"))
+FeatureScatter(object = s.obj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+dev.off()
+
+# VLN plot
+pdf(paste0("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/20hpf/pre-normalisation/CLEAN_", s.name, "_mito_vln_plot.pdf"))
+VlnPlot(object = s.obj, features = c("nFeature_RNA", "nCount_RNA", "percent.mito"), ncol = 3)
+dev.off()
---
+++ R/3.20hpf_normalisation.R
@@ -0,0 +1,27 @@
+library("Matrix")
+library("Seurat")
+library(tidyverse)
+library(dplyr)
+library(cowplot)
+
+setwd("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/data")
+
+#------------------------------------———MERGING—————————————————————---------------------------- 
+sample1 <- readRDS("S1a_hpf20_clean.rds")
+sample2 <- readRDS("S1b_hpf20_clean.rds")
+sample3 <- readRDS("S2_hpf20_clean.rds")
+
+combined <- merge(sample1, y = c(sample2, sample3), add.cell.ids = c("sample1", "sample2", "sample3"), project = "combined")
+
+# cell names have an added identifier 
+head(colnames(combined))
+
+# visualise
+table(combined$orig.ident)
+
+#------------------------------------———NORMALISATION—————————————————————---------------------------- 
+combined.normalised <- NormalizeData(object = combined, normalization.method = "LogNormalize", scale.factor = 10000)
+GetAssayData(combined.normalised[1:10, 1:15])
+
+# output file as .rds
+saveRDS(combined.normalised, file = "hpf20_combined_normalised.rds")
