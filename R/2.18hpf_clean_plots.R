12f0a422c72eff18247d76f9a0e2ad11dcd6d620
zxh755 <zxh755@student.bham.ac.uk>
2021-04-01 15:29:51 +0100

    undid loop

--- R/2.18hpf_clean_plots.R
+++ R/2.18hpf_clean_plots.R
@@ -9,22 +9,40 @@ setwd("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/data
 
 s.list <- list.files(pattern=glob2rx("*hpf18_clean.rds"))
 
-for(s in 1:length(s.list)){
-  s.name <- substr(s.list[s], 1,3)
-    if(grepl(s.name, pattern="_")){
-    s.name <- substr(s.list[s], 1,2)
-    }
-  print(s.name)
-  sobj <- readRDS(s.list[s])
+# S3
+s <- 1
+s.name <- substr(s.list[s], 1,3)
+  if(grepl(s.name, pattern="_")){
+  s.name <- substr(s.list[s], 1,2)
+  }
+  
+s.obj <- readRDS(s.list[s])
+  
 # RNA scatter plot 
-  pdf(paste0("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/18hpf/pre-normalisation/CLEAN_", s.name, "_RNA_scatter_plot.pdf"))
-  FeatureScatter(object = sobj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
-  dev.off()
+pdf(paste0("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/18hpf/pre-normalisation/CLEAN_", s.name, "_RNA_scatter_plot.pdf"))
+FeatureScatter(object = s.obj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+dev.off()
   
 # VLN plot
-  pdf(paste0("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/18hpf/pre-normalisation/CLEAN_", s.name, "_mito_vln_plot.pdf"))
-  VlnPlot(object = sobj, features = c("nFeature_RNA", "nCount_RNA", "percent.mito"), ncol = 3)
-  dev.off()
-  
-  print("done")
+pdf(paste0("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/18hpf/pre-normalisation/CLEAN_", s.name, "_mito_vln_plot.pdf"))
+VlnPlot(object = s.obj, features = c("nFeature_RNA", "nCount_RNA", "percent.mito"), ncol = 3)
+dev.off()
+
+# S4
+s <- 2
+s.name <- substr(s.list[s], 1,3)
+if(grepl(s.name, pattern="_")){
+  s.name <- substr(s.list[s], 1,2)
 }
+
+s.obj <- readRDS(s.list[s])
+
+# RNA scatter plot 
+pdf(paste0("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/18hpf/pre-normalisation/CLEAN_", s.name, "_RNA_scatter_plot.pdf"))
+FeatureScatter(object = s.obj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+dev.off()
+
+# VLN plot
+pdf(paste0("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/18hpf/pre-normalisation/CLEAN_", s.name, "_mito_vln_plot.pdf"))
+VlnPlot(object = s.obj, features = c("nFeature_RNA", "nCount_RNA", "percent.mito"), ncol = 3)
+dev.off()
