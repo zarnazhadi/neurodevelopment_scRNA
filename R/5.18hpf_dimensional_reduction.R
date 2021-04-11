8723f0f4929e5bc08a9f5aeecf9842223bc8e668
zxh755 <zxh755@student.bham.ac.uk>
2021-04-11 17:31:38 +0100

    added plots and titles

--- R/5.18hpf_dimensional_reduction.R
+++ R/5.18hpf_dimensional_reduction.R
@@ -3,34 +3,24 @@ library("Seurat")
 library(tidyverse)
 library(dplyr)
 library(cowplot)
+library(patchwork)
 
 setwd("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/data")
 sample <- readRDS("hpf18.cluster.rds")
 
-sample <- RunTSNE(object = sample, dims.use = 1:10, do.fast = TRUE)
+pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/18hpf/post-clustering/18hpf_jack_straw_plot.pdf")
+JackStrawPlot(object = sample, dims = 1:70, reduction = "pca", xmax = 0.0025) + NoLegend()
+dev.off()
+
+pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/18hpf/post-clustering/18hpf_elbow_plot.pdf")
+ElbowPlot(object = sample, ndims= 70)
+dev.off()
+
+sample <- RunTSNE(object = sample, dims.use = 1:70, do.fast = TRUE)
 
 pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/18hpf/post-clustering/18hpf_tSNE_plot.pdf")
-DimPlot(object = sample, reduction = "tsne")
+DimPlot(object = sample, reduction = "tsne") + plot_annotation(title = 'T-SNE Clusters for 70 PCs (18 hpf)')
 dev.off()
 
-sample <- RunUMAP(sample, reduction = "pca", dims = 1:20)
-
-# this doesnt work :(
-pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/18hpf/post-clustering/18hpf_UMAP.pdf")
-DimPlot(sample, reduction = "umap", split.by = "seurat_clusters") + RotatedAxis()
-dev.off()
-
-pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/other/other.pdf")
-#DotPlot(sample, features = "nCount_RNA", split.by = "seurat_clusters") + RotatedAxis()
-
-#DoHeatmap(sample, features = VariableFeatures(sample)[1:100], cells = 1:500, size = 1, 
-          #angle = 90) + NoLegend()
-
-#DimPlot(sample)
-
-#sample.no.tsne <- sample
-#sample.no.tsne[["tsne"]] <- NULL
-#DimPlot(sample.no.tsne) + RotatedAxis()
-
-DimPlot(sample, reduction = "umap")
-dev.off()
+# save as .rds file
+saveRDS(sample, file = "hpf18_tSNE.cluster.rds")
