1a9ff950ca6d86ed636f771a121f06448afd7eb5
zxh755 <zxh755@student.bham.ac.uk>
2021-04-11 17:32:00 +0100

    added plots and titles

--- R/5.20hpf_dimensional_reduction.R
+++ R/5.20hpf_dimensional_reduction.R
@@ -3,14 +3,23 @@ library("Seurat")
 library(tidyverse)
 library(dplyr)
 library(cowplot)
+library(patchwork)
 
 setwd("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/data")
 sample <- readRDS("hpf20.cluster.rds")
 
+pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/20hpf/post-clustering/20hpf_jack_straw_plot.pdf")
+JackStrawPlot(object = sample, dims = 1:70, reduction = "pca", xmax = 0.0025) + NoLegend()
+dev.off()
+
+pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/20hpf/post-clustering/20hpf_elbow_plot.pdf")
+ElbowPlot(object = sample, ndims= 70)
+dev.off()
+
 sample <- RunTSNE(object = sample, dims.use = 1:70, do.fast = TRUE)
 
 pdf("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/20hpf/post-clustering/20hpf_tSNE_plot.pdf")
-DimPlot(object = sample, reduction = "tsne")
+DimPlot(object = sample, reduction = "tsne") + plot_annotation(title = 'T-SNE Clusters for 70 PCs (20 hpf)')
 dev.off()
 
 # save as .rds file
