03cf1baaf4ef85c943e915b1cbc8e0344651db56
zxh755 <zxh755@student.bham.ac.uk>
2021-04-11 17:44:21 +0100

    new file

---
+++ sbatch/20hpf_DE_genes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --ntasks 5
+#SBATCH --time 60:00:00
+#SBATCH --qos bbdefault
+#SBATCH --mail-type ALL
+
+set -e
+
+module purge; module load bluebear
+module load bear-apps/2020a
+module load R/4.0.0-foss-2020a
+export R_LIBS_USER=${HOME}/R/library/${EBVERSIONR}/${BB_CPU}
+
+
+cd /rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/R # cd to R script
+
+# run Rscripts
+#Rscript 6.18hpf_DE_genes.R
+Rscript 6.20hpf_DE_genes.R
