e615d36521389e6c6b70a278f9c3e81359c211c5
zxh755 <zxh755@student.bham.ac.uk>
2021-04-13 15:52:15 +0100

    removed top 10 markers

--- R/6.18hpf_DE_genes.R
+++ R/6.18hpf_DE_genes.R
@@ -8,23 +8,15 @@ setwd("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/data
 sample <- readRDS("hpf18_tSNE.cluster.rds")
 sample.name <- "18hpf"
 
-# find markers for the first cluster 
-cluster1.markers <- FindMarkers(object = sample, ident.1 = 1, min.pct = 0.25)
-print(x = head(x = cluster1.markers, n = 5))
-
-# find all markers distinguishing cluster 5 from clusters 0 and 3
-cluster5.markers <- FindMarkers(object = sample, ident.1 = 2, ident.2 = c(0, 3), min.pct = 0.25)
-print(x = head(x = cluster5.markers, n = 5))
-
 # find markers for every cluster compared to all remaining cells, report
 # only the positive ones
-sample.markers <- FindAllMarkers(object = sample, only.pos = TRUE, min.pct = 0.25, thresh.use = 0.25)
+sample.markers <- FindAllMarkers(object = sample, only.pos = TRUE, min.pct = 0.18,  min.diff.pct = 0.15)
 
 # top two genes for each cluster
 markers <- sample.markers %>% group_by(cluster) %>% top_n(2, avg_log2FC)
 write.csv(markers, file = paste0(sample.name, "_top2_markers.csv"))
 
-for(c in seq(from= 0, to= 33)){
+for(c in seq(from= 0, to= 25)){
   top2 <- markers[markers$cluster == c,]$gene
   print(top2)
   
@@ -42,16 +34,3 @@ for(c in seq(from= 0, to= 33)){
   print (p)
   dev.off()
 }
-
-# find classification power
-cluster1.markers <- FindMarkers(object = sample, ident.1 = 0, thresh.use = 0.25, test.use = "roc", only.pos = TRUE)
-
-# top10 markers per cluster 
-top10 <- sample.markers %>% group_by(cluster) %>% top_n(10, avg_log2FC)
-write.csv(markers, file = paste0(sample.name, "_top10_markers.csv"))
-
-pdf(paste0("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/", sample.name, "/post-clustering/top10_markers_feature_plot.pdf"))
-p <- DoHeatmap(object = sample, features = top10$gene, label = TRUE) + 
-  theme(text = element_text(size = 5))
-print(p)
-dev.off()
