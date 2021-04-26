f932abe18b2534b7a170d5fc8126f22ed35c7fb6
zxh755 <zxh755@student.bham.ac.uk>
2021-04-26 17:33:11 +0100

    final commit

--- R/7.20hpf_cell_identities.R
+++ R/7.20hpf_cell_identities.R
@@ -7,7 +7,8 @@ library(data.table)
 
 setwd("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/data")
 sample.name <- "20hpf"
-sample <- readRDS(paste0("hpf20_tSNE.cluster.rds"))
+setwd("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/data/markers")
+sample <- readRDS("hpf20_tSNE.cluster.rds")
 top2 <- read.csv(file = paste0(sample.name, "_top2_markers.csv"), header = TRUE)
 top10 <- read.csv(file = paste0(sample.name, "_top10_markers.csv"), header = TRUE)
 atlas <- read.csv(file = paste0("atlas_", sample.name, ".csv"), header = TRUE)
@@ -33,25 +34,25 @@ for(row in seq(from= 1, to = dim(top10)[1])){
 }
 
 current.cluster.ids <- seq(from=0, to=40)
-new.cluster.ids <- c("Progenitors", "Eye (Cornea)/Cartilage", "Glutamatergic Neurons*",
-                     "GABAergic/Glutamatergic Neurons", "Neural Crest (Ectomesenchyme)", "Telencephalon", 
-                     "Hindbrain*", "Placode (Epibranchial)/Epidermal Ionocyte", "Optic Vesicle (Ventral)",
-                     "Radial Glia", "Progenitors (Eye)*", "Ventral Midbrain",
-                     "Midbrain (Progenitors)", "Ventral Diencephalon", "Rostral Blood Island (Myeloid)", 
-                     "Eye (Epidermis)", "Epidermis", "Telencephalon (Pallium, Glutamatergic Neuron)*",
-                     "RPE*", "Periderm*", "Heart Primordium",
+new.cluster.ids <- c("Progenitors", "Eye (Cornea)/Cartilage", "Glutamatergic Neurons*", #2
+                     "GABAergic/\nGlutamatergic Neurons", "Neural Crest (Ectomesenchyme),\nMesoderm", "Telencephalon", 
+                     "Hindbrain- R5/6", "Prechordal Plate/Polster", "Optic Vesicle (Ventral)",
+                     "Radial Glia", "Progenitors (Eye),\nPigment Cell (Iridophore)", "Ventral Midbrain",
+                     "Midbrain (Progenitors)", "Ventral Diencephalon", "Rostral Blood Island\n(Myeloid)", 
+                     "Eye (Epidermis)", "Epidermis", "Epidermis",
+                     "Telencephalon (Pallium, Neuron),\nGlutamatergic Neurons", "RPE", "Periderm", "Heart Primordium",
                      "Retina (Photoreceptor Precursor Cells)", "Macrophages", "Pharyngeal Arch (Posterior)", 
-                     "Epidermal Ionocyte", "Cartilage", "Telencephalon (Pallium, Neuron)*", 
-                     "28*", "Floor Plate", "Lens",
-                     "Glia", "Muscle", "Neural Crest*", 
+                     "Epidermal Ionocyte", "Cartilage", "Telencephalon (Pallium, Neuron),\nEpidermal Ionocyte", 
+                     "Progenitors (Eye)", "Floor Plate", "Lens",
+                     "Glia, Periderm", "Muscle", "Neural Crest*", 
                      "Retina (Muller Glia)", "Glutamatergic Neurons", "Oligodendrocytes",
-                     "Neuromast (Otic)", "Pigment Cell (Iridophore)", "Dermal Bone",
-                     "39", "Dorsal Habenula/Glutamatergic Neurons")
+                     "Neuromast (Otic)", "Pigment Cell (Iridophore)", "Dermal Bone", 
+                     "Dorsal Habenula/Glutamatergic Neurons")
 
 
 sample@active.ident <- plyr::mapvalues(x = sample@active.ident, from = current.cluster.ids, to = new.cluster.ids)
 
 pdf(paste0("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/", sample.name, "/post-clustering/NEW_tSNE_plot.pdf"))
-pp <- DimPlot(object = sample, reduction = "tsne", label = FALSE, pt.size = 0.5, label.size = 3) + theme_classic(base_size = 6) + guides(color = guide_legend(override.aes = list(size=1), ncol=2) )
+pp <- DimPlot(object = sample, reduction = "tsne", label = FALSE, pt.size = 0.5, label.size = 0.5) + theme_classic(base_size = 10) + guides(color = guide_legend(override.aes = list(size=1), ncol=2) )
 print(pp)
 dev.off()
