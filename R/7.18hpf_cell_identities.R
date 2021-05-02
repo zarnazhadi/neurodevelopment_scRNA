56ed67a58f046e21dce85847c57959a4c6a207b5
zxh755 <zxh755@student.bham.ac.uk>
2021-05-02 16:15:48 +0100

    final

--- R/7.18hpf_cell_identities.R
+++ R/7.18hpf_cell_identities.R
@@ -5,16 +5,19 @@ library(dplyr)
 library(cowplot)
 library(data.table)
 
-setwd("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/data")
-sample <- readRDS(paste0("hpf18_tSNE.cluster.rds"))
+# import Seurat object and markers
+setwd("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/data/markers")
+sample <- readRDS("hpf18_tSNE.cluster.rds")
 sample.name <- "18hpf"
 top2 <- read.csv(file = paste0(sample.name, "_top2_markers.csv"), header = TRUE)
 top10 <- read.csv(file = paste0(sample.name, "_top10_markers.csv"), header = TRUE)
 
+# import gene annotation resource
 setwd("/rds/projects/v/vianaj-development-rna/appDir/data")
 atlas <- as.data.frame(fread("ZFBrainAtlasMaster.tsv", header = TRUE))
 atlas <- atlas %>% filter(STAGE == "18 hpf")
 
+# assign cell identities to markers
 identities.list <- list()
 for(row in seq(from= 1, to = dim(top2)[1])){
   identity <- toString(atlas[atlas$`ENRICHED MARKERS` %like% top2[row,8], ]$`ASSIGNED CELL TYPE/STATE`)
@@ -25,6 +28,9 @@ for(row in seq(from= 1, to = dim(top2)[1])){
   top2$cell.identity[[row]] <- identity
   identities.list[[row]] <- identity
 }
+
+top2 <- apply(top2,2,as.character)
+write.csv(as.data.frame(top2), file = paste0(sample.name, "_top2_marker_names.csv"))
 
 identities.list <- list()
 for(row in seq(from= 1, to = dim(top10)[1])){
@@ -37,9 +43,15 @@ for(row in seq(from= 1, to = dim(top10)[1])){
   identities.list[[row]] <- identity
 }
 
+top10 <- top10 %>%
+  filter(!(str_detect(cell.identity, "\\d")))
+top10 <- apply(top10,2,as.character)
+write.csv(top10, file = paste0(sample.name, "_top10_marker_names.csv"))
+
+# manual insertion of cluster identities 
 current.cluster.ids <- seq(from=0, to=25)
 new.cluster.ids <- c("Hindbrain (Floor plate/Boundary- R3),\nVentral Diencephalon, Dienc-Midbrain Boundary,\nDiencephalon", "Hindbrain (Ant. Dorsal, Rhombic Lip)", "2", 
-                     "Telencephalon (I, II)", "Optic Vesicle (Dorsal)", "Hindbrain- R7, Pharangeal Arch (Posterior),\nVentral Midbrain",
+                     "Telencephalon (I, II)", "Optic Vesicle (Dorsal)", "Hindbrain- R7, Pharyngeal Arch (Posterior),\nVentral Midbrain",
                      "Floor Plate", "Ventral Diencephalon", "Epidermis", 
                      "Placode (Epibranchial/Lens),\nEpidermis", "Midbrain, Midbrain Neural Rod", "Neural Crest", 
                      "Hindbrain- R5/6", "Diencephalon", "Placode (Epibranchial)",
@@ -49,8 +61,10 @@ new.cluster.ids <- c("Hindbrain (Floor plate/Boundary- R3),\nVentral Diencephalo
                      "Prechordal Plate/Polster", "Blood Precursors")
 sample@active.ident <- plyr::mapvalues(x = sample@active.ident, from = current.cluster.ids, to = new.cluster.ids)
 
+# output t-SNE plot
 pdf(paste0("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/plots/", sample.name, "/post-clustering/NEW_tSNE_plot.pdf"))
 pp <- DimPlot(object = sample, reduction = "tsne", label = FALSE, pt.size = 0.5, label.size = 2) + theme_classic(base_size = 10) + 
   guides(color = guide_legend(override.aes = list(size=1), ncol=1))
 print(pp)
 dev.off()
+
