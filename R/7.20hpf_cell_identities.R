90f2b5b92fb6e88bfd71a1ca8c7a3374b54268cb
zxh755 <zxh755@student.bham.ac.uk>
2021-05-02 16:15:57 +0100

    final

--- R/7.20hpf_cell_identities.R
+++ R/7.20hpf_cell_identities.R
@@ -7,8 +7,8 @@ library(data.table)
 
 setwd("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/data")
 sample.name <- "20hpf"
-setwd("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/data/markers")
 sample <- readRDS("hpf20_tSNE.cluster.rds")
+setwd("/rds/projects/v/vianaj-development-rna/Zarnaz/neurodevelopment_scRNA/data/markers")
 top2 <- read.csv(file = paste0(sample.name, "_top2_markers.csv"), header = TRUE)
 top10 <- read.csv(file = paste0(sample.name, "_top10_markers.csv"), header = TRUE)
 atlas <- read.csv(file = paste0("atlas_", sample.name, ".csv"), header = TRUE)
@@ -22,16 +22,23 @@ for(row in seq(from= 1, to = dim(top2)[1])){
   top2$cell.identity[[row]] <- identity
 }
 
+top2 <- apply(top2,2,as.character)
+write.csv(as.data.frame(top2), file = paste0(sample.name, "_top2_marker_names.csv")) 
+
 identities.list <- list()
 for(row in seq(from= 1, to = dim(top10)[1])){
   identity <- toString(atlas[atlas$ENRICHED.MARKERS %like% top10[row,8], ]$ASSIGNED.CELL.TYPE.STATE)
   if(identity == ""){
     identity <- toString(top10[row, 7])
-    print(identity)
   }
   top10$cell.identity[[row]] <- identity
   identities.list[[row]] <- identity
 }
+
+top10 <- top10 %>%
+  filter(!(str_detect(cell.identity, "\\d")))
+top10 <- apply(top10,2,as.character)
+write.csv(top10, file = paste0(sample.name, "_top10_marker_names.csv"))
 
 current.cluster.ids <- seq(from=0, to=40)
 new.cluster.ids <- c("Progenitors", "Eye (Cornea)/Cartilage", "Glutamatergic Neurons*", #2
